d8ab31ebaaee67e4387a874b45bd3a24d698d66e
Bas <bas@giantfox.nl>
2023-05-11 14:02:01 +0200

    [add] triangle opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "tigl.hpp"
 #include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+#include "tigl.hpp"
 using tigl::Vertex;
 
 #pragma comment(lib, "glfw3.lib")
@@ -11,59 +12,69 @@ using tigl::Vertex;
 GLFWwindow* window;
 
 void init();
-void update();
-void draw();
 
-int main(void)
-{
-    if (!glfwInit())
-        throw "Could not initialize glwf";
-    window = glfwCreateWindow(1400, 800, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        throw "Could not initialize glwf";
-    }
-    glfwMakeContextCurrent(window);
-
-    tigl::init();
-
-    init();
-
-	while (!glfwWindowShouldClose(window))
-	{
-		update();
-		draw();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
-
-
-    return 0;
+static int CompileShader(const std::string& source, unsigned int type){
+	unsigned int id  = glCreateShader(GL_VERTEX_SHADER);
 }
 
-
-void init()
-{
-    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
-    {
-        if (key == GLFW_KEY_ESCAPE)
-            glfwSetWindowShouldClose(window, true);
-    });
+static int CreateShader(const std::string& vertexShader,
+                        const std::string& fragmentShader) {
+  unsigned int program = glCreateProgram();
+	unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
+	
 
 }
 
+int main(void) {
+  if (!glfwInit())
+    throw "Could not initialize glwf";
 
-void update()
-{
+  std::cout << glewInit() << std::endl;
 
+  window = glfwCreateWindow(1400, 800, "Hello World", NULL, NULL);
+
+  if (!window) {
+    glfwTerminate();
+    throw "Could not initialize glwf";
+  }
+
+  glfwMakeContextCurrent(window);
+
+  std::cout << glewInit() << std::endl;
+
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  init();
+
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  unsigned int buffer = 0;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawElements(GL_TRIANGLES, 3,0 );
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
 }
 
-void draw()
-{
-    glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void init() {
+  glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode,
+                                int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE)
+      glfwSetWindowShouldClose(window, true);
+  });
 }
-
